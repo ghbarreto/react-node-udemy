07c876f89701fbdf609879eb27a56c220294eb9d
Gabriel Barreto <gh.barreto@hotmail.com>
2021-04-26 09:27:18 -0300

    a

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,11 +2,9 @@
 // 3XgJBdIp9ecBdRmhkHz4NYII client secret
 
 module.exports = {
-  googleClientID:
-    "125734171955-gbmdc17nkfi224frtk8a5kn56k8b9e7f.apps.googleusercontent.com",
-  googleClientSecret: "3XgJBdIp9ecBdRmhkHz4NYII",
-  mongoURI:
-    "mongodb+srv://ghbr:ybEMRtnAvjoavJdH@emaily.vlnan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  googleClientID: "",
+  googleClientSecret: "",
+  mongoURI: "",
   cookieKey: "redacted",
 };
 
