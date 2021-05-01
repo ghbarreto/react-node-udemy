aa59f15695dbfc989f5050af90b018a161d78cff
Gabriel Barreto <gh.barreto@hotmail.com>
2021-05-01 14:41:52 -0300

    adding production features

--- server/index.js
+++ server/index.js
@@ -33,6 +33,24 @@ require("./routes/authRoutes")(app);
 // initializing the billing routes
 require("./routes/billingRoutes")(app);
 
+// this will only run when the app is in production
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like main.js file, or main.css file.
+
+  // if any get request comes in for any routes and
+  //the app cannot find it, then look into the client
+  //and check if the route will be in there
+  app.use(express.static("client/build"));
+
+  // Express will render the index.html file if it
+  // doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname), "client", "build", "index.html");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
